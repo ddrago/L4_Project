96d4fb54b8aa201f0cbe5682c9c062f369ce7baa
ddrago <dragodiego286@gmail.com>
2022-02-04 10:56:33 +0000

    Error handling in case the AudioManager.Play method is called and given the string of an audio file that does not exist or has not been added to the AudioManager.sounds attribute.

--- Assets/Bose/Wearable/Scripts/AudioManager.cs
+++ Assets/Bose/Wearable/Scripts/AudioManager.cs
@@ -23,6 +23,11 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s  = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound file " + name + " not found!");
+            return;
+        }
         Debug.Log("Now playing: " + s.name);
         s.source.Play();
     }
