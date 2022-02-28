422d811d47908605a7ce7883323dd7bb02c685e7
ddrago <dragodiego286@gmail.com>
2022-02-28 13:48:50 +0000

    Brought the instantiation of the menu items into a CreateMenuLayout function. This answers to an enum public variable that shows as a drop down slection menu on the unity editor inspector. This allows me to pick a menu layout and the menu items will be created accordingly. For now only horizontal is available.

--- Assets/Bose/Wearable/ExampleApp/Demos/Basic/Scripts/Spawner.cs
+++ Assets/Bose/Wearable/ExampleApp/Demos/Basic/Scripts/Spawner.cs
@@ -8,23 +8,51 @@ public class Spawner : MonoBehaviour
     public int Quantity;
     public int Radius = 40;
 
+    public enum Layout
+    {
+        horizontal,
+        vertical,
+        pie
+    };
+
+    //Create the variable field
+    [SerializeField] public Layout layout;
+
+    public void CreateMenuLayout()
+    {
+        float ratio = 1f / Quantity;
+        float itemSize = (Radius * 2) / Quantity;
+
+        int nameNum = 1;
+
+        switch (layout)
+        {
+            case Layout.horizontal:
+
+                for (int i = 1 - Quantity; i <= Quantity - 1; i += 2)
+                {
+                    Vector3 pos = new Vector3(i * ratio * Radius, 0, 15);
+                    GameObject menuItemTest = Instantiate(MenuItemPrefab, pos, transform.rotation);
+
+                    menuItemTest.transform.localScale = new Vector3(itemSize, Radius, 1);
+                    menuItemTest.name = "Item" + nameNum;
+                    nameNum++;
+                }
+
+                break;
+            case Layout.vertical:
+                print("vertical");
+                break;
+            case Layout.pie:
+                print("pie");
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        float ratio = 1f / Quantity;
-        float width = (Radius * 2) / Quantity;
-
-        int nameNum = 1;
-        for (int i = 1-Quantity; i <= Quantity-1; i+=2)
-        {
-            Vector3 pos = new Vector3(i*ratio*Radius, 0, 15);
-            GameObject menuItemTest = Instantiate(MenuItemPrefab, pos, transform.rotation);
-
-            menuItemTest.transform.localScale = new Vector3(width, Radius, 1);
-            menuItemTest.name = "Item" + nameNum;
-            nameNum++;
-        }
-
+        CreateMenuLayout();
     }
 
     // Update is called once per frame
